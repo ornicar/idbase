97d6d006a69ae9b490c08be61ced2f8a0d637607
Thibault Duplessis <thibault.duplessis@gmail.com>
2014-01-28 23:20:56 +0100

    upgrade vendors

--- project/Build.scala
+++ project/Build.scala
@@ -8,15 +8,15 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     // Add your project dependencies here,
-    "org.reactivemongo" %% "reactivemongo" % "0.10.1-PRISMIC",
-    "org.reactivemongo" %% "play2-reactivemongo" % "0.10.1-PRISMIC",
-    "org.pegdown" % "pegdown" % "1.4.1",
+    "org.reactivemongo" %% "reactivemongo" % "0.10.0",
+    "org.reactivemongo" %% "play2-reactivemongo" % "0.10.2",
+    "org.pegdown" % "pegdown" % "1.4.2",
     "jp.t2v" %% "play2-auth" % "0.11.0",
     "ssu" %% "ssu" % "0.1-SNAPSHOT"
   )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    scalaVersion := "2.10.2",
+    scalaVersion := "2.10.3",
     resolvers ++= Seq(
       "Sonatype Snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/",
       "iliaz.com" at "http://scala.iliaz.com/"
